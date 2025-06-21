cb822dcce25e3dd232a5006167a3fc1cd7af9f59
sofiaas16 <sarahzambrano2019@gmail.com>
2025-06-21 18:35:40 -0500

    feat: :sparkles: Consultas Sencillas

--- dql_select.sql
+++ dql_select.sql
@@ -14,3 +14,29 @@ join historial_pago historial
 on t.tarjeta_id = historial.tarjeta_id
 where c.nombre =  'Mateo';
 
+
+SELECT cm.*
+FROM cuota_manejo cm 
+WHERE MONTH(cm.fecha_cuota) = 3;
+
+select c.nombre, tt.descuento
+from tarjeta t
+join cliente c 
+on t.cliente_id = c.cliente_id
+join cuota_manejo cm 
+on t.tarjeta_id = cm.tarjeta_id
+join tipo_tarjeta tt
+on t.tipo_tarjeta_id = tt.tipo_tarjeta_id
+where tt.descuento = '15.00'
+group by c.nombre;
+
+
+select cm.fecha_cuota, t.tarjeta_id, t.numero_tarjeta
+from cuota_manejo cm
+join tarjeta t
+on cm.tarjeta_id = t.tarjeta_id
+join historial_pago historial
+on t.tarjeta_id = historial.tarjeta_id
+where month (cm.fecha_cuota) = 3;
+
+
